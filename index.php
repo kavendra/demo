de341cf2845a1992905acfd19efa878ed83714db
kavendra <kavendra.chauhan12@gmail.com>
2017-08-13 13:29:04 +0530

    update

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ $username = "root";
 $password = "";
 $dbname = "test";
 
+// Create connection to mysql
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
